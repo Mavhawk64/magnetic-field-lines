f1e1da6aa9bc7f65706ddef9f83817ea5f97f166
Maverick <mavberk@gmail.com>
2024-04-12 23:04:43 -0400

    Simplified the Project and included Daniel Shiffman's Flow Field as Inspiration

--- magnet.js
+++ magnet.js
@@ -1,11 +1,146 @@
 // magnet.js
+// Maverick Berkland
 class Magnet {
-    constructor(x, y, strength, north_vector) {
-        this.x = x;
-        this.y = y;
+    // horse-shoe magnet
+    constructor(x, y, angle, strength, flipped = false) {
+        this.pos = createVector(x, y);
+        this.angle = angle;
         this.strength = strength;
-        this.north_vector = north_vector;
-        // normalize the north vector
-        this.north_vector.normalize();
+        this.flipped = flipped;
+    }
+
+    isMouseOver() {
+        // Calculate the position of the mouse relative to the center of the magnet
+        let x = mouseX - this.pos.x;
+        let y = mouseY - this.pos.y;
+
+        // Reverse the angle to account for the rotation of the magnet
+        let angle = -this.angle;
+
+        // Rotate the point back, which essentially unrotates the rectangle area
+        let x1 = x * cos(angle) - y * sin(angle);
+        let y1 = x * sin(angle) + y * cos(angle);
+
+        // Check if the mouse is within the bounds of the unrotated rectangle
+        let isOver = x1 > -40 && x1 < 40 && y1 > -80 && y1 < 0; // Adjusted for correct height bounds
+
+        return isOver;
+    }
+
+
+    setPosition(x, y) {
+        this.pos.set(x, y);
+    }
+
+    update() {
+        this.draw();
+    }
+
+    getLeftBounds(flip) {
+        if (flip) {
+            return this.getRightBounds(false);
+        }
+        let bounds = [];
+        bounds.push(createVector(-40, -80));
+        bounds.push(createVector(-20, -80));
+        bounds.push(createVector(-20, -40));
+        for (let i = 180; i >= 90; i -= 10) {
+            let x = 20 * cos(radians(i));
+            let y = 20 * sin(radians(i));
+            bounds.push(createVector(x, y - 40));
+        }
+        bounds.push(createVector(0, 0));
+        for (let i = 90; i <= 180; i += 10) {
+            let x = 40 * cos(radians(i));
+            let y = 40 * sin(radians(i));
+            bounds.push(createVector(x, y - 40));
+        }
+        return bounds;
+    }
+
+    getRightBounds(flip) {
+        if (flip) {
+            return this.getLeftBounds(false);
+        }
+        let bounds = [];
+        bounds.push(createVector(40, -80));
+        bounds.push(createVector(20, -80));
+        bounds.push(createVector(20, -40));
+        for (let i = 0; i <= 90; i += 10) {
+            let x = 20 * cos(radians(i));
+            let y = 20 * sin(radians(i));
+            bounds.push(createVector(x, y - 40));
+        }
+        bounds.push(createVector(0, 0));
+        for (let i = 90; i >= 0; i -= 10) {
+            let x = 40 * cos(radians(i));
+            let y = 40 * sin(radians(i));
+            bounds.push(createVector(x, y - 40));
+        }
+        return bounds;
+    }
+
+    draw() {
+        /**
+         *     x xx x    x xx x
+         *     x    x    x    x
+         *     x    x    x    x
+         *     x     xxxx     x
+         *      x            x
+         *       x x x x x x 
+         */
+        push();
+        translate(this.pos.x, this.pos.y);
+        rotate(this.angle);
+        scale(this.flipped ? -1 : 1, 1);
+        fill(255, 0, 0);
+        stroke(0);
+        strokeWeight(2);
+
+        // Left side
+        beginShape();
+        for (let v of this.getLeftBounds(false)) {
+            vertex(v.x, v.y);
+        }
+        endShape(CLOSE);
+
+        // Right side
+        fill(0, 0, 255);
+        beginShape();
+        for (let v of this.getRightBounds(false)) {
+            vertex(v.x, v.y);
+        }
+        endShape(CLOSE);
+        noFill();
+        // rectMode(CENTER);
+        // rect(0, -40, 80, 80);
+        pop();
+
+        push();
+        translate(this.pos.x, this.pos.y);
+        rotate(this.angle);
+        fill(255);
+        textSize(16);
+        text('N', -30 * (this.flipped ? -1 : 1) - 5, -60);
+        text('S', 30 * (this.flipped ? -1 : 1) - 5, -60);
+        pop();
+    }
+
+    getFieldAtPoint(px, py) {
+        let northPole = createVector(-30 * (this.flipped ? -1 : 1), -60).rotate(this.angle).add(this.pos);
+        let southPole = createVector(30 * (this.flipped ? -1 : 1), -60).rotate(this.angle).add(this.pos);
+        let point = createVector(px, py);
+
+        let toNorth = p5.Vector.sub(point, northPole);
+        let toSouth = p5.Vector.sub(point, southPole);
+
+        // Calculate field vector from north pole (assuming it behaves like a positive monopole)
+        let fieldNorth = p5.Vector.div(toNorth, pow(toNorth.mag(), 3)).mult(this.strength);
+
+        // Calculate field vector from south pole (assuming it behaves like a negative monopole)
+        let fieldSouth = p5.Vector.div(toSouth, pow(toSouth.mag(), 3)).mult(-this.strength);
+
+        // Superpose fields from both poles
+        return p5.Vector.add(fieldNorth, fieldSouth);
     }
 }
--- sketch.js
+++ sketch.js
@@ -7,15 +7,101 @@
  */
 
 let magnets = [];
-
+let draggedMagnet = null;
+let scl = 20;
+let particles = [];
+let flowfield = [];
+let cols, rows;
+let prevFrameStop = 0;
 function setup() {
-    createCanvas(400, 400);
-    magnets[0] = new BarMagnet(200, 200, 50, 200, 1, createVector(0, -1));
+    createCanvas(800, 800);
+
+    createParticles();
+
+    // for (let i = 0; i < 2; i++) {
+    //     magnets.push(new Magnet(random(80, width - 80), random(80, height - 80), random(TWO_PI), random(1, 10) * 0.1, random() > 0.5));
+    // }
+    magnets.push(new Magnet(400, 200, PI, 1, false));
+    magnets.push(new Magnet(400, 600, 0, 1, true));
+    background(51);
 }
 
 function draw() {
-    background(220);
+    // background(51);
+    drawFieldVecs();
+    if (frameCount - prevFrameStop < 40) {
+        for (let particle of particles) {
+            particle.follow(flowfield);
+            particle.update();
+            particle.edges();
+            particle.show();
+        }
+    }
     for (let magnet of magnets) {
         magnet.update();
     }
+}
+
+function createParticles() {
+    particles = [];
+    for (let i = 0; i < 200; i++) {
+        for (let j = 0; j < 200; j++) {
+            particles.push(new Particle(i * 4, j * 4, random() > 0.5));
+        }
+    }
+}
+
+function drawFieldVecs() {
+    let spacing = scl; // Space between field lines
+    cols = floor(width / spacing);
+    rows = floor(height / spacing);
+    flowfield = new Array(cols * rows);
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * spacing;
+            let y = j * spacing;
+            let totalField = createVector(0, 0);
+
+            for (let magnet of magnets) {
+                let field = magnet.getFieldAtPoint(x, y);
+                totalField.add(field);
+            }
+
+            let old_mag = totalField.mag() * 1e5;
+            old_mag = map(old_mag, 0, 1, 0, 255);
+            totalField.setMag(spacing / 2); // Normalize vector length for visualization
+            push();
+            translate(x, y);
+            stroke(0, 0, 0, old_mag);
+            line(0, 0, totalField.x, totalField.y);
+            pop();
+            let index = i + j * cols;
+            flowfield[index] = totalField;
+        }
+    }
+}
+
+function mousePressed() {
+    for (let magnet of magnets) {
+        if (magnet.isMouseOver()) {
+            draggedMagnet = magnet; // Set the currently dragged magnet
+            particles = [];
+            break;
+        }
+    }
+}
+
+function mouseDragged() {
+    if (draggedMagnet) {
+        draggedMagnet.setPosition(mouseX, mouseY);
+        background(51);
+        draw();
+    }
+}
+
+function mouseReleased() {
+    draggedMagnet = null; // Clear the dragged magnet when the mouse is released
+    createParticles();
+    prevFrameStop = frameCount;
 }
